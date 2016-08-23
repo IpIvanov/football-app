17bce4cfb09ff929063cc9ccaa8d1b6dda64878b
Ivan Ivanov <Ivan Ivanov>
2016-08-23 21:19:45 +0300

    Added predictions and angular layout framework

--- src/app/predictions/predictions.controller.js
+++ src/app/predictions/predictions.controller.js
@@ -1,55 +1,26 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('football')
-    .controller('PredictionsController', PredictionsController);
+    angular
+        .module('football')
+        .controller('PredictionsController', PredictionsController);
 
-  /** @ngInject */
-  function PredictionsController(footballData) {
-    var vm = this;
+    /** @ngInject */
+    function PredictionsController(footballData) {
+        var vm = this;
+        vm.predictions = [];
 
-    vm.fixtures = [];
-    vm.competitions = [];
-    vm.livescoreMatches = [];
-    vm.gridOptions = {
-      excludeProperties: '__metadata',
-    };
+        activate();
 
-    activate();
+        function activate() {
+            getPredictions();
+        }
 
-    function activate() {
-      footballData.getFootballData('http://api.football-data.org/v1/fixtures?timeFrame=n2').then(function (result) {
-        vm.fixtures = result.fixtures;
-        vm.gridOptions.data = result.fixtures;
-      });
-    }
-
-    function getAllCompetitions() {
-      footballData.getFootballData('http://api.football-data.org/v1/competitions').then(function (result) {
-        vm.competitions = result;
-        getCompetitionId(vm.fixtures);
-      });
-    }
-
-    function getCompetitionId(fixtures) {
-      var competitionsIds = [];
-      angular.forEach(fixtures, function (fixture) {
-        competitionsIds.push(fixture._links.competition.href.split('/').pop());
-      });
-      getCompetitionNames(competitionsIds, vm.competitions);
-    }
-
-    function getCompetitionNames(competitionsIds, arrayOfNames) {
-      vm.arrayOfNames = [];
-      angular.forEach(competitionsIds, function (competitionId) {
-        angular.forEach(arrayOfNames, function (name) {
-          if (competitionId === name.id.toString()) {
-            vm.arrayOfNames.push(name.caption.slice(0, -8));
-          }
-        });
-      });
+        function getPredictions() {
+            footballData.getLiveScoresData('http://ipivanov.com/bettingapp/get_predictions.php').then(function (result) {
+                vm.predictions = result.data;
+            });
+        }
 
     }
-  }
 })();
